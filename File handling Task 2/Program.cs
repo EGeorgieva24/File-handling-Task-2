098d0d9e23f5b41d08a3a84601b68545080b5073
Elitsa Georgeva <Elitsa Georgeva@DESKTOP-8D1ST4F>
2023-10-02 20:59:05 +0300

    Add project files.

---
+++ File handling Task 2/Program.cs
@@ -0,0 +1,15 @@
+﻿class Test
+{
+    public static void Main()
+    {
+        string path = @"D:\Programming guysss\File_Handling";
+        DirectoryInfo Directory = new DirectoryInfo(path);
+        Directory.Create();
+
+        var file = $@"{path}\Steve.txt";
+        
+        File.AppendAllText(file,"hello");
+
+   
+    }
+}
